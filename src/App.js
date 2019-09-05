af7267041441c0a1b3a9d391198d0a3a1268ee67
Mohammad Baragbah <duniandewon@gmail.com>
2019-09-05 19:43:59 +0700

    Quick setup
    Setup file structure and import necessary files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+import logo from "./assets/img/logo.svg";
+
+const App = () => (
+  <Fragment>
+    <h1>Hello</h1>
+    <img src={logo} alt="Logo" />
+  </Fragment>
+);
+
+export default App;
